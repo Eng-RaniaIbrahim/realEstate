8ce6170e72c82ec6690a5216c43ce5a4e89dc874
Rania_Ibrahim <raniaibrahim1112001@gmail.com>
2023-03-29 23:09:28 +0200

    final upload

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function () {
+    $('.rania').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows:false,
+        rtl: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }]
+    });
+});
